2b555100c08e4d7ea330210830b97fe07bce3ded
nikita011297 <nikitagupta1298@gmail.com>
2024-07-05 13:58:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://google.com")
+
+time.sleep(10)
+
+driver.quit()
